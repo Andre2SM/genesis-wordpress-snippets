266a6aae6e9ce2f286f4a94c03ed880d5ea68014
Andre2SM <sm.aandree@gmail.com>
2019-01-13 19:12:43 -0500

    added snippets for visual studio code

--- snippet-sidebar.php
+++ snippet-sidebar.php
@@ -12,11 +12,16 @@ register_sidebar( array(
     'after_title'   => '</h2>',
 ));
 
-//Call a sidebar conditionally
+//Call a sidebar conditionally using wordpress standard
 
 if ( is_active_sidebar( 'sidebar-1' ) ) {
     dynamic_sidebar( 'sidebar-1' );
 }
 
+////Call a sidebar conditionally using wordpress genesis
+
+if ( is_active_sidebar( 'sidebar-1' ) ) {
+    genesis_widget_area( 'sidebar-1' );
+}
 
 ?>
