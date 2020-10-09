250ad04bf5c61e19c7a1d1523565a84f87d82800
Andree Sabas <sm.aandree@gmail.com>
2020-10-09 12:41:05 -0500

    Added image property to Article schema

--- snippets-yoast/schema-article-default-image.php
+++ snippets-yoast/schema-article-default-image.php
@@ -10,33 +10,17 @@
  * 
  */
 
-function as_schema_article_default_image( $data ) {
+function sa_schema_default_article_image( $data ) {	
 
-	$permalink	= get_permalink();
-	$language	= str_replace('_', '-', get_locale() );	
-	$image_src	= wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+	$image_src 		= wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
 
-	if ( !empty($image_src) && $image_src[1] > 1199 ) {
+	if ( empty($image_src) && $image_src[1] < 1199 ) {
 		
-		$image_url	= $image_src[0];
-		$image_width	= $image_src[1];
-		$image_height	= $image_src[2];
-
-	} else {
-
-		$image_url	= 'IMAGE_URL'; // Replace with your own image url. Image should be at least 1200 pixels wide.
-		$image_width	= 'IMAGE_WIDTH';
-		$image_height	= 'IMAGE_HEIGHT';
+		$data['image']['type']	= 'imageObject';
+		$data['image']['url']	= 'IMAGE_URL';
 
 	}
-	
-	$data['primaryImageOfPage']['type']		= 'ImageObject';
-	$data['primaryImageOfPage']['@id']		= $permalink.'#primaryimage';
-	$data['primaryImageOfPage']['url']		= $image_url;
-	$data['primaryImageOfPage']['inLanguage']	= $language;
-	$data['primaryImageOfPage']['width']		= $image_width;
-	$data['primaryImageOfPage']['height']		= $image_height;
 
 	return $data;
 }
-add_filter( 'wpseo_schema_article', 'as_schema_article_default_image' );
+add_filter( 'wpseo_schema_article', 'sa_schema_default_article_image' );
